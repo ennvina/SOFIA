baefb9bdbe6e0568a5f2a09f3a91fa0d2dfda02b
ennvina <vinnywow@aim.com>
2023-07-23 17:27:20 +0200

    Cleaned up console output

--- src/main.lua
+++ src/main.lua
@@ -3,7 +3,8 @@ local AddonName, SOFIA = ...
 SLASH_SOFIA1 = "/sofia"
 
 local function printColor(color, pattern, ...)
-    print(WrapTextInColorCode(string.format(pattern, unpack{...}), color))
+    local addonColor = SOFIA:GetColorHex('darkRed')
+    print(string.format('|c%s%s|r: ', addonColor, AddonName)..WrapTextInColorCode(string.format(pattern, unpack{...}), color))
 end
 
 local function printChat(pattern, ...)
@@ -12,15 +13,15 @@ local function printChat(pattern, ...)
 end
 
 function SOFIA.Error(self, pattern, ...)
-    local color = "FFB20000" -- Do not fetch from SOFIA:GetColorHex to minimize calls during errors
-    printColor(color, '[%s] '..pattern, AddonName, ...)
+    local hcColor = "FFB20000" -- Do not fetch from SOFIA:GetColorHex to minimize calls during errors
+    print(string.format('|c%s%s|r: '..pattern, hcColor, AddonName, unpack{...}))
 end
 
 function SOFIA.Debug(self, pattern, ...)
     if not self.db or not self.db.debug then return end
 
-    local color = "FFB20000" -- Do not fetch from SOFIA:GetColorHex to minimize calls during debug
-    printColor(color, '[%s] '..pattern, AddonName, ...)
+    local hcColor = "FFB20000" -- Do not fetch from SOFIA:GetColorHex to minimize calls during debug
+    print(string.format('|c%s%s|r: '..pattern, hcColor, AddonName, unpack{...}))
 end
 
 SlashCmdList.SOFIA = function(msg, editBox)
@@ -28,38 +29,37 @@ SlashCmdList.SOFIA = function(msg, editBox)
         SOFIADB = nil -- reset to nothing
         SOFIA:LoadDB() -- because DB has nothing, loading will set it to default
         SOFIA:ApplySettings()
-        printChat("%s options and data have been reset.", AddonName)
+        printChat("Options and data have been reset.")
     elseif msg == "show" then
         SOFIA:ShowWindow()
-        printChat("%s window shown. If you still don't see it, please try /sofia reset", AddonName)
+        printChat("Window shown. If you still don't see it, please try /sofia reset")
     elseif msg == "hide" then
         SOFIA:HideWindow()
-        printChat("%s window hidden. To show it again, please enter /sofia show", AddonName)
+        printChat("Window hidden. To show it again, please enter /sofia show")
     elseif msg == "toggle" then
         SOFIA:ToggleWindow()
-        printChat("%s window toggled.", AddonName)
+        printChat("Window toggled.")
     elseif msg == "debug" then
         SOFIA:ToggleDebug()
     else
         printChat("Usage: /sofia [command]")
-        printChat(
-            "Commands:\n"..
-            "show: show the window\n"..
-            "hide: hide the window\n"..
-            "reset: reset to default settings and wipe all data\n")
+        printChat("Commands:"..
+        "\n".."show: show the window"..
+        "\n".."hide: hide the window"..
+        "\n".."reset: reset to default settings and wipe all data")
     end
 end
 
 function SOFIA.ToggleDebug(self)
     local color = SOFIA:GetColorHex('chat')
     if not self.db then
-        printChat("%s DB not initialized yet.", AddonName)
+        printChat("DB not initialized yet.")
     end
     if self.db.debug then
         self.db.debug = false
-        printChat("%s debug disabled.", AddonName, 123)
+        printChat("Debug disabled.")
     else
         self.db.debug = true
-        printChat("%s debug enabled.", AddonName, 345)
+        printChat("Debug enabled.")
     end
 end
